0688658a71a8c80b04a43b7aa8728fae3b36e123
Clinton Wee <Clinton.Wee@softwire.com>
2023-04-20 12:50:22 +0100

    Turned each variant of update_quality into its own static function

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,5 +1,30 @@
 # -*- coding: utf-8 -*-
 
+def update_quality_normal(item):
+    if item.sell_in >= 0:
+        item.quality -= 1
+    else:
+        item.quality -= 2
+
+
+def update_quality_cheese(item):
+    if item.sell_in < 0:
+        item.quality += 2
+    else:
+        item.quality += 1
+
+
+def update_quality_tickets(item):
+    if item.sell_in <= 1:
+        item.quality = 0
+    elif item.sell_in <= 5:
+        item.quality += 3
+    elif item.sell_in <= 10:
+        item.quality += 2
+    else:
+        item.quality += 1
+
+
 class GildedRose(object):
 
     def __init__(self, items):
@@ -26,35 +51,23 @@ class GildedRose(object):
     def update_quality(self):
         for item in self.items:
             if item.name in self.cheeses:
-                if item.sell_in <= 0:
-                    item.quality += 2
-                else:
-                    item.quality += 1
+                update_quality_cheese(item)
 
             elif item.name in self.tickets:
-                if item.sell_in <= 1:
-                    item.quality = 0
-                elif item.sell_in <= 5:
-                    item.quality += 3
-                elif item.sell_in <= 10:
-                    item.quality += 2
-                else:
-                    item.quality += 1
+                update_quality_tickets(item)
 
             elif item.name in self.legendary:
                 continue
 
             else:
-                if item.sell_in > 0:
-                    item.quality -= 1
-                else:
-                    item.quality -= 2
+                update_quality_normal(item)
 
             self.enforce_max_min_quality(item)
 
     def next_day(self):
-        self.update_quality()
         self.update_sell_in()
+        self.update_quality()
+
 
 
 class Item:
