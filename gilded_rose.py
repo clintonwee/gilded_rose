6d0b8ea717e00eb1eb75fea76f9305aeb02fd704
Clinton Wee <Clinton.Wee@softwire.com>
2023-04-20 13:03:10 +0100

    Separated update_quality functions for each category into new file

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,35 +1,6 @@
 # -*- coding: utf-8 -*-
 
-def update_quality_normal(item):
-    if item.sell_in >= 0:
-        item.quality -= 1
-    else:
-        item.quality -= 2
-
-
-def update_quality_cheese(item):
-    if item.sell_in < 0:
-        item.quality += 2
-    else:
-        item.quality += 1
-
-
-def update_quality_tickets(item):
-    if item.sell_in <= 1:
-        item.quality = 0
-    elif item.sell_in <= 5:
-        item.quality += 3
-    elif item.sell_in <= 10:
-        item.quality += 2
-    else:
-        item.quality += 1
-
-
-def update_quality_conjured(item):
-    if item.sell_in >= 0:
-        item.quality -= 2
-    else:
-        item.quality -= 4
+from update_quality import *
 
 
 class GildedRose(object):
